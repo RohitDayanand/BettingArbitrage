174f40538e68b5af2514397459e0feb05d33044b
raihanzaman <130304624+raihanzaman@users.noreply.github.com>
2025-04-09 12:40:05 -0400

    Update polymarket_scraper.py

--- polymarket_scraper.py
+++ polymarket_scraper.py
@@ -1,13 +1,20 @@
 import time
 import sqlite3
 import random
+import sys
+# Selenium Libraries and Methods
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+from urllib3.exceptions import ReadTimeoutError
+# Web Driver Libaries
 from webdriver_manager.chrome import ChromeDriverManager
+# Local Files
 from standardize_names import standardizeColumnNames
-
-import sys
 
 # Function to scrape data
 def scrape_polymarket(url, event_id, table_created, driver):
@@ -16,23 +23,39 @@ def scrape_polymarket(url, event_id, table_created, driver):
     except Exception as e:
         print("Error loading page", e)
         return
-    time.sleep(6)  # Allow time for page to load
+    wait = WebDriverWait(driver, 60)
+
+    # Wait until the element is present in the DOM
+    element = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'c-dhzjXW') and contains(@class, 'c-dhzjXW-ifipQUC-css')]"))
+    )
 
     # Extract event name and standardize it
     table_name = standardizeColumnNames(event_id)
     
-    print(table_name)
-    
     # Locate the container div for all markets
-    markets_containers = driver.find_elements(By.XPATH, "//div[contains(@class, 'c-iuGBSs')]")
+    markets_containers = driver.find_elements(By.XPATH, "//div[contains(@class, 'c-dhzjXW') and contains(@class, 'c-dhzjXW-ifipQUC-css')]")
     print(f"Found {len(markets_containers)} markets")
     
     for market in markets_containers:
         market_name = standardizeColumnNames(market.find_element(By.XPATH, ".//p[contains(@class, 'c-cZBbTr')]").text)
-        yes_price = market.find_element(By.XPATH, ".//div[contains(text(), 'Buy Yes')]").text.split()[-1].replace('¢', '')
-        no_price = market.find_element(By.XPATH, ".//div[contains(text(), 'Buy No')]").text.split()[-1].replace('¢', '')
-        this_trading_volume = market.find_element(By.XPATH, ".//p[contains(., 'Vol.')]").text
-       
+        try: 
+            yes_price = market.find_element(By.XPATH, ".//div[contains(text(), 'Buy Yes')]").text.split()[-1].replace('¢', '')
+        except StaleElementReferenceException:
+            print("Stale element issue with yes, we will refresh and find it next time")
+            return
+        try:
+            no_price = market.find_element(By.XPATH, ".//div[contains(text(), 'Buy No')]").text.split()[-1].replace('¢', '')
+        except StaleElementReferenceException:
+            print("Stale element issue with no, we will refresh and find it next time")
+            return
+
+        try:
+            this_trading_volume = market.find_element(By.XPATH, ".//p[contains(., 'Vol.')]").text
+        except StaleElementReferenceException:
+            print("Stale element issue with trading volume, we will refresh and find it next time")
+            return
+
         this_trading_volume = this_trading_volume.split()[0].replace('$', '').replace(',', '')
 
         try:
@@ -78,11 +101,29 @@ def polymarket_scraper(event_url, event_id, iterations, driver):
     table_created = False
     counter = 0
     while counter < iterations:
-        scrape_polymarket(event_url, event_id, table_created, driver)  # Replace with actual event name
-        counter += 1
-        driver.refresh()
-        time.sleep(10 + int(random.uniform(5,10)))  # Run every minute
-
+        if counter % 3 == 0:
+            try:
+                driver.refresh()
+                scrape_polymarket(event_url, event_id, table_created, driver)  # Replace with actual event name
+                counter += 1
+            except ReadTimeoutError:
+                driver.quit()
+                options = webdriver.ChromeOptions()
+                prefs = {
+                    "profile.managed_default_content_settings.images": 2
+                }
+                
+                options.add_experimental_option("prefs", prefs)
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--headless")  # Add this line to make the browser headless
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+                driver.set_page_load_timeout(180)  # Sets a 3-minute timeout for page load.
+                scrape_polymarket(event_url, event_id, table_created, driver)
+                counter += 1
+        else:
+            scrape_polymarket(event_url, event_id, table_created, driver)
+            counter += 1
+            time.sleep(10 + int(random.uniform(5,10)))  # Run every minute
     driver.quit()
 
 def main():
