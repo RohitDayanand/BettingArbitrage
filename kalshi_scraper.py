8a3ba33ddb994b8632da75423c18422d7c07da10
raihanzaman <130304624+raihanzaman@users.noreply.github.com>
2025-04-14 11:14:34 -0400

    Update kalshi_scraper.py

--- kalshi_scraper.py
+++ kalshi_scraper.py
@@ -10,7 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from urllib3.exceptions import ReadTimeoutError
 from standardize_names import standardizeColumnNames
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 
 def init_db(event_name):
     conn = sqlite3.connect("scraper_demo1.db")
@@ -31,20 +35,28 @@ def init_db(event_name):
 
 # Function to scrape data
 
-def scrape_polymarket(url, event_id, table_created, driver):
+def scrape_polymarket(url, event_id, table_created, driver, isRefreshed):
     try:
-        driver.get(url)
+        if driver.current_url != url:
+            driver.get(url)
+        else:
+            print("Already on the desired url no need to refresh")
     except Exception as e:
         print("Error loading page", e)
         return
-    time.sleep(6)  # Allow time for page to load
-    # Click the "More Markets" button if it exists
+    wait = WebDriverWait(driver, 60)
+   
+    new_element = wait.until(
+        EC.presence_of_element_located((By.XPATH, "//div[@style='flex: 1 1 0%;']"))
+        )
+    
     button_elements = driver.find_elements(By.XPATH, "//div[@style='flex: 1 1 0%;']")
     try:
         more_market_button = button_elements[-1]
         print(more_market_button.text)
         first_clickable = more_market_button.find_element(By.XPATH, ".//*[self::button or self::a or self::span or @role='button' or @onclick]")
-        first_clickable.click()
+        if isRefreshed:
+            first_clickable.click()
         time.sleep(2)
     except NoSuchElementException as e:
         print("No more markets button")
@@ -84,6 +96,7 @@ def scrape_polymarket(url, event_id, table_created, driver):
                     no_price = 0
 
         print(f"Event: {market_name_text} | Yes: {yes_price} | No: {no_price}")
+        print("timestamp is", datetime.now())
         # Close the driver and database connection
 
         conn = sqlite3.connect(f"{event_id}_kalshi.db")
@@ -113,13 +126,66 @@ def scrape_polymarket(url, event_id, table_created, driver):
 def initatizeKalshiScrape(event_id, event_url, iterations, driver):
     table_created = False
     counter = 0
-
     while counter < iterations:
-        scrape_polymarket(event_url, event_id, table_created, driver)  # Replace with actual event name
-        counter += 1
-        driver.refresh()
-        time.sleep(10 + int(random.uniform(5,10)))  # Run every minute
-
+        
+        #Don't refresh too fast, page should automatically change data because it is javascript based 
+        if counter % 3 == 0:
+            try:
+                driver.refresh()
+                scrape_polymarket(event_url, event_id, table_created, driver, True)
+                counter += 1
+            except ReadTimeOutError:
+                print("Read time out error - restarting scraper")
+                driver.quit()
+                options = webdriver.ChromeOptions()
+                prefs = {
+                    "profile.managed_default_content_settings.images": 2
+                }
+                
+                options.add_experimental_option("prefs", prefs)
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--headless")  # Add this line to make the browser headless
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+                driver.set_page_load_timeout(65)  # Sets a 3-minute timeout for page load.
+                scrape_polymarket(event_url, event_id, table_created, driver, True)
+                counter += 1
+            except TimeoutError as ex:
+                print("Time out exception at t-60 seconds from last printed timestamp")
+                driver.quit()
+                options = webdriver.ChromeOptions()
+                prefs = {
+                    "profile.managed_default_content_settings.images": 2
+                }
+                
+                options.add_experimental_option("prefs", prefs)
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--headless")  # Add this line to make the browser headless
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+                driver.set_page_load_timeout(65)  # Sets a 3-minute timeout for page load.
+                scrape_polymarket(event_url, event_id, table_created, driver, True)
+                counter += 1
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("Restarting application...")
+                time.sleep(3)  # Optional: Add a delay before restarting
+                print("Time out exception at t-60 seconds from last printed timestamp")
+                driver.quit()
+                options = webdriver.ChromeOptions()
+                prefs = {
+                    "profile.managed_default_content_settings.images": 2
+                }
+                
+                options.add_experimental_option("prefs", prefs)
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--headless")  # Add this line to make the browser headless
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+                driver.set_page_load_timeout(65)  # Sets a 3-minute timeout for page load.
+                scrape_polymarket(event_url, event_id, table_created, driver, True)
+                counter += 1
+        else:
+            time.sleep(10 + int(random.uniform(5,10)))
+            scrape_polymarket(event_url, event_id, table_created, driver, False)
+            counter += 1
     driver.quit()
 
 def main():
@@ -144,13 +210,18 @@ def main():
             return
         if not iterations:
             print("Please provide a valid number of iterations")
-        if iterations <= 0 or iterations > 1000:
+        if iterations <= 0 or iterations > 9999:
             print("Please provide a valid number of iterations, or not too many")
             return
     options = webdriver.ChromeOptions()
+    prefs = {
+    "profile.managed_default_content_settings.images": 2
+    }
+    options.add_experimental_option("prefs", prefs)
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--headless")  # Add this line to make the browser headless
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.set_page_load_timeout(65)  # Sets a 3-minute timeout for page load.
     initatizeKalshiScrape(event_id, event_url, iterations, driver)
 
 if __name__ == "__main__":
